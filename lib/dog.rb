26b86bb9ab66aaaeb82fb30a9e2a7b6cb36b14d5
Mitzi Estrada <m_estrada17@live.com>
2020-02-25 06:34:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @name
   end
   
-  def name
+  def name(name)
     @name= name
   end
   
@@ -11,7 +11,7 @@ class Dog
     @breed
   end
   
-  def breed
+  def breed(breed)
     @breed= breed
   end
   
